f728912ca308ec9a0f3e2fb0635e4f5a6d6e666a
urlastway <andriy.semenchenko13@gmail.com>
2022-04-27 14:47:34 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//Ask name
+const name = prompt("Come ti chiami?")
+//Ask last name
+const last_name = prompt("Qual'è il tuo cognome?")
+//Ask favorite color
+const favorite_color = prompt("Qual'è il tuo colore preferito")
+//Replay
+let number = "21"
+document.getElementById("title").innerHTML = name + " " + last_name + " " + favorite_color + " " + number
